eaf0740c38dd053314e6ca433e85eed458806911
mazebeat <>
2017-03-02 01:51:04 +0000

    Add timeline

---
+++ src/main/java/cl/intelidata/beans/TimelineBean.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2016, Intelidata S.A.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package cl.intelidata.beans;
+
+import cl.intelidata.jpa.Hitos;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.timeline.TimelineSelectEvent;
+import org.primefaces.model.timeline.TimelineEvent;
+import org.primefaces.model.timeline.TimelineModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author DFeliu
+ */
+@ManagedBean
+@SessionScoped
+public class TimelineBean implements Serializable {
+
+    private static final long serialVersionUID = -2152389656664659476L;
+    private static Logger logger = LoggerFactory.getLogger(TimelineBean.class);
+
+    private TimelineModel model;
+
+    private boolean selectable = true;
+    private boolean zoomable = true;
+    private boolean moveable = true;
+    private boolean stackEvents = true;
+    private String eventStyle = "box";
+    private boolean axisOnTop;
+    private boolean showCurrentTime = true;
+    private boolean showNavigation = false;
+
+    @ManagedProperty(value = "#{loginBean}")
+    private LoginBean loginbean;
+
+    public LoginBean getLoginbean() {
+        return loginbean;
+    }
+
+    public void setLoginbean(LoginBean loginbean) {
+        this.loginbean = loginbean;
+    }
+
+    @PostConstruct
+    public void init() {
+        fillTimeline();
+    }
+
+    public void fillTimeline() {
+        try {
+            model = new TimelineModel();
+            List<Hitos> h = loginbean.getClient().getHitosList();
+
+            for (Hitos hitos : h) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(hitos.getFecha());
+                model.add(new TimelineEvent(hitos.getHito(), cal.getTime()));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    public void onSelect(TimelineSelectEvent e) {
+        TimelineEvent timelineEvent = e.getTimelineEvent();
+
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected event:", timelineEvent.getData().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public TimelineModel getModel() {
+        return model;
+    }
+
+    public boolean isSelectable() {
+        return selectable;
+    }
+
+    public void setSelectable(boolean selectable) {
+        this.selectable = selectable;
+    }
+
+    public boolean isZoomable() {
+        return zoomable;
+    }
+
+    public void setZoomable(boolean zoomable) {
+        this.zoomable = zoomable;
+    }
+
+    public boolean isMoveable() {
+        return moveable;
+    }
+
+    public void setMoveable(boolean moveable) {
+        this.moveable = moveable;
+    }
+
+    public boolean isStackEvents() {
+        return stackEvents;
+    }
+
+    public void setStackEvents(boolean stackEvents) {
+        this.stackEvents = stackEvents;
+    }
+
+    public String getEventStyle() {
+        return eventStyle;
+    }
+
+    public void setEventStyle(String eventStyle) {
+        this.eventStyle = eventStyle;
+    }
+
+    public boolean isAxisOnTop() {
+        return axisOnTop;
+    }
+
+    public void setAxisOnTop(boolean axisOnTop) {
+        this.axisOnTop = axisOnTop;
+    }
+
+    public boolean isShowCurrentTime() {
+        return showCurrentTime;
+    }
+
+    public void setShowCurrentTime(boolean showCurrentTime) {
+        this.showCurrentTime = showCurrentTime;
+    }
+
+    public boolean isShowNavigation() {
+        return showNavigation;
+    }
+
+    public void setShowNavigation(boolean showNavigation) {
+        this.showNavigation = showNavigation;
+    }
+}
